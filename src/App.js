e39d49218dcada02ea1e3df6d0f808017d07eec7
zglawless <zakglawless@gmail.com>
2023-06-21 19:56:47 -0500

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Home from './Home';
+import Profile from './profile'
+import {BrowserRouter,Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path = "/" element={<Home/>}>
+        </Route>
+        <Route path = "/profile" element={<Profile/>}>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+   
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.css",
+    "./src/index.css",
+    "./src/Home.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
